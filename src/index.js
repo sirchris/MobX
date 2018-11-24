6076fc58274d11e158d3024c0b1ce80150bb4bb3
sirchris <florkowski.krzysztof@gmail.com>
2018-11-25 00:16:40 +0100

    Strict mode

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Root from './containers/Root';
+import { configure } from 'mobx';
+
+configure({ enforceActions: 'always' });
 
 ReactDom.render(<Root />, document.getElementById('root'));
